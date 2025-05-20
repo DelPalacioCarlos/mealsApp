81df0144ff8c1302d44c7e8a5e286e5debc72c4f
DelPalacioCarlos <carlosdelpalacio5@gmial.com>
2025-05-19 19:50:06 -0700

    cambio01

---
+++ components/MealItem.js
@@ -0,0 +1,71 @@
+import { View, Text, Image, Pressable, StyleSheet, Platform } from "react-native";
+import { useNavigation} from '@react-navigation/native'
+import MealDetails from "./MealDetails"
+
+function MealDetails({ id, title, imageUrl, duration, complexty, affordatibility }){
+    console.log('title', title)
+    console.log('imageUrl', imageUrl)
+
+    const navigation = useNavigation()
+
+    function selectMealItemHandler (){
+        navigation.navigate('MealDetail', {mealId: id})
+    }
+
+    return(
+        <View style={Styles.mealItem}>
+            <Pressable
+            android_ripple={{color: '#ccc'}}
+            style={({ pressed }) => (pressed ? styles.buttonPressed: null) }
+            onPress={selectMealItemHandler}
+            >
+                <View>
+                    <View>
+                        <Image
+                        source={{uri: imageUrl}}
+                        style={styles.image}
+                        />
+                        <Text style={styles.title}>
+                            {title}
+                        </Text>
+                    </View>
+                    <MealDetails
+                    duration={duration}
+                    complexty={complexty}
+                    affordatibility={affordatibility}
+                    />
+                </View>
+            </Pressable>
+        </View>
+    )
+}
+
+export default MealItem
+
+const styles = StyleSheet.create({
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: Platform.OS === 'android' ? "hidden" : 'visible',
+        backgroundColor: 'white',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 8,
+    },
+    buttonPressed: {
+        opacity: 0.5
+    },
+    innerContainer: {
+        borderRadius: 8,
+        overflow: 'hidden',
+        height: 200
+    },
+    title:{
+        fontWeight:'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        margin: 8
+    },
+})
